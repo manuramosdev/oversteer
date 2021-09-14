4789b1e3aeaa8da61b402e4a80e40f66de928abf
Manu Ramos <mail@manuramos.dev>
2021-09-14 18:59:24 +0200

    hilt dependencies

---
+++ app/src/main/java/dev/manuramos/oversteer/OversteerApplication.kt
@@ -0,0 +1,8 @@
+package dev.manuramos.oversteer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OversteerApplication: Application() {
+}
--- app/src/main/java/dev/manuramos/oversteer/di/AppModule.kt
+++ app/src/main/java/dev/manuramos/oversteer/di/AppModule.kt
@@ -1,4 +1,36 @@
 package dev.manuramos.oversteer.di
 
-class AppModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.manuramos.oversteer.data.remote.ErgastApi
+import dev.manuramos.oversteer.data.repository.DriverRepositoryImpl
+import dev.manuramos.oversteer.domain.repository.DriverRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+const val BASE_URL = "https://ergast.com/api/f1/"
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideErgastApi(): ErgastApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ErgastApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDriverRepository(api: ErgastApi): DriverRepository {
+        return DriverRepositoryImpl(api)
+    }
 }
