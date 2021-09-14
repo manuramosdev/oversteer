413151a42c27321b2ab50ad83e899ac6a4002cb0
Manu Ramos <mail@manuramos.dev>
2021-09-14 16:51:33 +0200

    constructor ids

--- app/src/main/java/dev/manuramos/oversteer/domain/model/Constructor.kt
+++ app/src/main/java/dev/manuramos/oversteer/domain/model/Constructor.kt
@@ -17,10 +17,13 @@ data class ConstructorStanding(
 
 val COLORS = mapOf(
     "mercedes" to 0xFF00D2BE,
+    "red_bull" to 0xFF0600EF,
     "alpine" to 0xFF0090FF,
     "alphatauri" to 0xFF2B4562,
     "williams" to 0xFF005AFF,
+    "aston_martin" to 0xFF006F62,
     "ferrari" to 0xFFDC0000,
+    "alfa" to 0xFF900000,
     "haas" to 0xFFFFFFFF,
     "mclaren" to 0xFFFF8700,
 
