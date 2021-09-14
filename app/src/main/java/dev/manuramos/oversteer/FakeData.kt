0cd4124f9bf77a39f31fc43564967f4088148366
Manu Ramos <mail@manuramos.dev>
2021-09-14 19:57:03 +0200

    fake data changes

--- app/src/main/java/dev/manuramos/oversteer/FakeData.kt
+++ app/src/main/java/dev/manuramos/oversteer/FakeData.kt
@@ -1,27 +1,23 @@
 package dev.manuramos.oversteer
 
-import dev.manuramos.oversteer.model.Constructor
-import dev.manuramos.oversteer.model.Driver
-import dev.manuramos.oversteer.model.DriverStanding
-import okhttp3.internal.http.toHttpDateString
-import java.util.*
+import dev.manuramos.oversteer.domain.model.Constructor
+import dev.manuramos.oversteer.domain.model.Driver
+import dev.manuramos.oversteer.domain.model.DriverStanding
 
-object FAKE_DATA {
-    val hamilton = Driver("hamilton", "Lewis", "Hamilton", "British", 44, "HAM", "", Date().toHttpDateString())
-    val bottas = Driver("bottas", "Valtteri", "Bottas", "Finnish", 77, "BOT", "", Date().toHttpDateString())
-    val russell = Driver("russell", "George", "Russell", "British", 63, "RUS", "", Date().toHttpDateString())
-    val sainz = Driver("sainz", "Carlos", "Sainz", "Spanish", 55, "SAI", "", Date().toHttpDateString())
-    val alonso06 = Driver("alonso", "Fernando", "Alonso", "Spanish", 14, "ALO", "", Date().toHttpDateString())
+val hamilton = Driver("hamilton", "Lewis", "Hamilton", "British", 44, "HAM", "", "")
+val bottas = Driver("bottas", "Valtteri", "Bottas", "Finnish", 77, "BOT", "", "")
+val russell = Driver("russell", "George", "Russell", "British", 63, "RUS", "", "")
+val sainz = Driver("sainz", "Carlos", "Sainz", "Spanish", 55, "SAI", "", "")
+val alonso06 = Driver("alonso", "Fernando", "Alonso", "Spanish", 14, "ALO", "", "")
 
-    val mercedes = Constructor("mercedes", "", "Mercedes", "British")
-    val ferrari = Constructor("ferrari", "", "Ferrari", "Italian")
-    val williams = Constructor("williams", "", "Williams", "British")
-    val renault = Constructor("renault", "", "Renault", "French")
-    val driverStandings: List<DriverStanding> = listOf(
-        DriverStanding(1, 200, 5, hamilton, listOf(mercedes)),
-        DriverStanding(2, 150, 4, bottas, listOf(mercedes)),
-        DriverStanding(3, 120, 3, russell, listOf(williams, mercedes)),
-        DriverStanding(4, 80, 2, sainz, listOf(ferrari)),
-        DriverStanding(5, 55, 1, alonso06, listOf(renault)),
-    )
-}
+val mercedes = Constructor("mercedes", "", "Mercedes", "British")
+val ferrari = Constructor("ferrari", "", "Ferrari", "Italian")
+val williams = Constructor("williams", "", "Williams", "British")
+val renault = Constructor("renault", "", "Renault", "French")
+val driverStandings: List<DriverStanding> = listOf(
+    DriverStanding(1, 200, 5, hamilton, listOf(mercedes)),
+    DriverStanding(2, 150, 4, bottas, listOf(mercedes)),
+    DriverStanding(3, 120, 3, russell, listOf(williams, mercedes)),
+    DriverStanding(4, 80, 2, sainz, listOf(ferrari)),
+    DriverStanding(5, 55, 1, alonso06, listOf(renault)),
+)
